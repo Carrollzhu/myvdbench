670287e0e5c4fe5cffa7d1021230b502ce7c21d7
Carroll <zhuxj.gz@gmail.com>
2020-04-10 12:34:27 +0800

    Add files via upload

---
+++ profiles/mount.sh
@@ -0,0 +1,16 @@
+ssh 172.16.85.181 mount 172.16.85.133:/ifs/isilon /mnt/isilon
+ssh 172.16.85.182 mount 172.16.85.133:/ifs/isilon /mnt/isilon
+ssh 172.16.85.183 mount 172.16.85.133:/ifs/isilon /mnt/isilon
+ssh 172.16.85.184 mount 172.16.85.133:/ifs/isilon /mnt/isilon
+ssh 172.16.85.185 mount 172.16.85.134:/ifs/isilon /mnt/isilon
+ssh 172.16.85.186 mount 172.16.85.134:/ifs/isilon /mnt/isilon
+ssh 172.16.85.187 mount 172.16.85.134:/ifs/isilon /mnt/isilon
+ssh 172.16.85.188 mount 172.16.85.134:/ifs/isilon /mnt/isilon
+ssh 172.16.85.189 mount 172.16.85.135:/ifs/isilon /mnt/isilon
+ssh 172.16.85.190 mount 172.16.85.135:/ifs/isilon /mnt/isilon
+ssh 172.16.85.193 mount 172.16.85.135:/ifs/isilon /mnt/isilon
+ssh 172.16.85.196 mount 172.16.85.135:/ifs/isilon /mnt/isilon
+ssh 172.16.85.197 mount 172.16.85.136:/ifs/isilon /mnt/isilon
+ssh 172.16.85.198 mount 172.16.85.136:/ifs/isilon /mnt/isilon
+ssh 172.16.85.199 mount 172.16.85.136:/ifs/isilon /mnt/isilon
+ssh 172.16.85.200 mount 172.16.85.136:/ifs/isilon /mnt/isilon
